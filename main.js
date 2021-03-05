abed06c79b0cff72e6961e58855829fc667502e7
Mighty Alex <perfectflow@live.com>
2021-03-05 02:39:38 +0100

    Added option to round values instead of the default rounding

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function create() {
     let strokeCheck = window.localStorage.getItem("stroke") == "true" ? "checked" : "";
     let cornersCheck = window.localStorage.getItem("corners") == "true" ? "checked" : "";
     let shadowCheck = window.localStorage.getItem("shadow") == "true" ? "checked" : "";
+    let roundCheck = window.localStorage.getItem("round") == "true" ? "checked" : "";
     let strokeImageDisplay = window.localStorage.getItem("stroke") == "true" ? "inline" : "none";
     let cornersImageDisplay = window.localStorage.getItem("corners") == "true" ? "inline" : "none";
     let shadowImageDisplay = window.localStorage.getItem("shadow") == "true" ? "inline" : "none";
@@ -21,7 +22,8 @@ function create() {
             #preview #strokeImage {display: `+strokeImageDisplay+`; z-index: 3; position: absolute; left: 0; top: 0;}
             #preview #cornersImage {display: `+cornersImageDisplay+`; z-index: 4; position: absolute; left: 0; top: 0;}
             #options {width: 100%; display: block;}
-            #options ul li {width: 100%; display: flex; flex-direction: row; justify-content: space-between;}
+            #options ul li {width: 100%; display: flex; flex-direction: row; justify-content: space-between; align-items: center;}
+            #options ul li.separator {width: 100%; height: 0px; border-top: 1px solid #ddd; margin: 10px 0px;}
             #options ul li h3 {margin: 0;}
             button {width: 100%;}
         </style>
@@ -42,6 +44,8 @@ function create() {
                     <li><h3>Stroke</h3><input type="checkbox" class="updateSetting" name="stroke" `+strokeCheck+` /></li>
                     <li><h3>Corners</h3><input type="checkbox" class="updateSetting" name="corners" `+cornersCheck+` /></li>
                     <li><h3>Shadow</h3><input type="checkbox" class="updateSetting" name="shadow" `+shadowCheck+` /></li>
+                    <li class="separator"></li>
+                    <li><h3>Round values</h3><input type="checkbox" class="updateSetting" name="round" `+roundCheck+` /></li>
                 </ul>
             </div>
             <br/><br/>
@@ -93,7 +97,7 @@ async function update() {
                     selectionHistory[item.guid].width = item.localBounds.width;
                     selectionHistory[item.guid].height = item.localBounds.height;
                     selectionHistory[item.guid].scale = finalScale;
-                    console.log(selectionHistory[item.guid].width, selectionHistory[item.guid].height, selectionHistory[item.guid].scale);
+                    // console.log(selectionHistory[item.guid].width, selectionHistory[item.guid].height, selectionHistory[item.guid].scale);
                 }
             }else{
                 // save
@@ -102,7 +106,7 @@ async function update() {
                 myArray['width'] = item.localBounds.width;
                 myArray['height'] = item.localBounds.width;
                 myArray['scale'] = 1;
-                console.log(myArray);
+                // console.log(myArray);
                 selectionHistory[item.guid] = myArray;
             }
         });
@@ -113,28 +117,51 @@ async function update() {
 
 function scaleAttributes(){
     selection.items.forEach(function (item) {
-        let rawScale = selectionHistory[item.guid].scale;
-        let scale = (Math.round(rawScale * 2) / 2).toFixed(1);
-
+        let scale = selectionHistory[item.guid].scale;
         if(window.localStorage.getItem("stroke") == "true"){
-            item.strokeWidth = item.strokeWidth * scale;
+            if(item.strokeEnabled){
+                console.log("here 1");
+                if(window.localStorage.getItem("round") == "true"){
+                    console.log(item.strokeWidth, scale);
+                    item.strokeWidth = rnd(item.strokeWidth * scale);
+                }else{
+                    item.strokeWidth = item.strokeWidth * scale;
+                }  
+            }
         }
         if(window.localStorage.getItem("corners") == "true"){
             if(item.hasRoundedCorners){
-                item.cornerRadii = { 
-                    topLeft: item.cornerRadii.topLeft * scale, 
-                    topRight: item.cornerRadii.topRight * scale, 
-                    bottomRight: item.cornerRadii.bottomRight * scale, 
-                    bottomLeft: item.cornerRadii.bottomLeft * scale
-                };
+                if(window.localStorage.getItem("round") == "true"){
+                    item.cornerRadii = { 
+                        topLeft: rnd(item.cornerRadii.topLeft * scale),
+                        topRight: rnd(item.cornerRadii.topRight * scale),
+                        bottomRight: rnd(item.cornerRadii.bottomRight * scale),
+                        bottomLeft: rnd(item.cornerRadii.bottomLeft * scale)
+                    };
+                }else{
+                    item.cornerRadii = { 
+                        topLeft: item.cornerRadii.topLeft * scale, 
+                        topRight: item.cornerRadii.topRight * scale, 
+                        bottomRight: item.cornerRadii.bottomRight * scale, 
+                        bottomLeft: item.cornerRadii.bottomLeft * scale
+                    };
+                }
             }
         }
         if(window.localStorage.getItem("shadow") == "true"){
             if(item.shadow){
-                item.shadow = new Shadow(item.shadow.x * scale, item.shadow.y * scale, item.shadow.blur * scale, item.shadow.color);
+                if(window.localStorage.getItem("round") == "true"){
+                    item.shadow = new Shadow(rnd(item.shadow.x * scale), rnd(item.shadow.y * scale), rnd(item.shadow.blur * scale), item.shadow.color);
+                }else{
+                    item.shadow = new Shadow(item.shadow.x * scale, item.shadow.y * scale, item.shadow.blur * scale, item.shadow.color);
+                }
             }
         }
     });
+}
+
+function rnd(number){
+    return parseInt((Math.ceil(number * 2) / 2).toFixed(1));
 }
 
 module.exports = {
